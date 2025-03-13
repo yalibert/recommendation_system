50935bcfc24ecc05bb5d4bac2cd99e66fc940bda
Yann Alibert <42999305+yalibert@users.noreply.github.com>
2025-03-13 19:56:41 +0100

    Update generate_own_database.py

--- pages/generate_own_database.py
+++ pages/generate_own_database.py
@@ -335,16 +335,16 @@ if st.button("Search Papers"):
     elif not author_list:
         st.error("Please enter at least one author name.")
     else:
-        papers_by_author = get_author_papers(author_list,ads_api_key)
-        weight_paper = compute_weights(papers_by_author)
-        all_papers_dict = make_all_papers_dict(papers_by_author)
-        st.write('Now downloading all abstract cited. This will take a while...')
-        dict_papers,json_data = download_and_embed_papers(all_papers_dict)
-        # Provide a download button
-        st.download_button(
-            label="Download JSON file",
-            data=json_data,
-            file_name="dict_papers_cited_by_author.json",
-            mime="application/json"
-        )
+        #papers_by_author = get_author_papers(author_list,ads_api_key)
+        #weight_paper = compute_weights(papers_by_author)
+        #all_papers_dict = make_all_papers_dict(papers_by_author)
+        #st.write('Now downloading all abstract cited. This will take a while...')
+        #dict_papers,json_data = download_and_embed_papers(all_papers_dict)
+        ## Provide a download button
+        #st.download_button(
+        #    label="Download JSON file",
+        #    data=json_data,
+        #    file_name="dict_papers_cited_by_author.json",
+        #    mime="application/json"
+        #)
 
